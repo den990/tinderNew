57c121ef05af670c02a742cf6d22f3ec65dc25c4
amfunNyLORD <funnybulletgames@yandex.ru>
2023-11-18 18:20:46 +0300

    Исправил стили на странице редактирования профиля, небольшие изменения на экране регистрации

--- views/site/profile.php
+++ views/site/profile.php
@@ -25,20 +25,23 @@ $this->title = "Profile";
     <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
     <div class="profile-editing__fields">
         <div class="profile-editing__fields__text-fields">
-            <div>
+            <div class="profile-editing__fields__text-fields__name-block">
                 <?= $form->field($model, 'first_name')->textInput(['placeholder' => 'Имя', 'class' => 'profile-editing__fields__text-fields__input-text'])->label(false) ?>
                 <?= $form->field($model, 'last_name')->textInput(['placeholder' => 'Фамилия', 'class' => 'profile-editing__fields__text-fields__input-text input-text_big field-padding'])->label(false) ?>
             </div>
-            <div>
+            <div class="profile-editing__fields_text-fields_additional-info">
                 <?= $form->field($model, 'gender')->dropDownList(['' => 'Пол', 'Мужской' => 'Мужской', 'Женский' => 'Женский'], ['class' => 'profile-editing__fields__text-fields__input-select'])->label(false) ?>
                 <?= $form->field($model, 'birthday')->input('date', ['class' => 'profile-editing__fields__text-fields__date field-padding'])->label(false) ?>
             </div>
 
             <?= $form->field($model, 'location')->dropDownList(['' => 'Город', 'Йошкар-Ола' => 'Йошкар-Ола', 'Москва' => 'Москва', 'Санкт-Петербург' => 'Санкт-Петербург', 'Челябинск' => 'Челябинск'], ['class' => 'profile-editing__fields__text-fields__input-select__city'])->label(false) ?>
 
-            <?= $form->field($model, 'description')->textInput(['placeholder' => 'Дополнительная информация', 'class' => 'profile-editing__fields__text-fields__input-text big-text'])->label(false) ?>
+            <?= $form->field($model, 'description')->textarea(['placeholder' => 'Дополнительная информация', 'class' => 'profile-editing__fields__text-fields__input-text big-text', 'rows' => '6'])->label(false) ?>
         </div>
-        <?= $form->field($model, 'photo')->fileInput()->label(false) ?>
+        <div class="profile-editing__fields__photo-button-block">
+            <?= $form->field($model, 'photo')->fileInput()->label(false) ?>
+            <?= Html::submitButton('Сохранить', ['class' => 'profile-editing__fields__photo-button-block__submit-button']) ?>
+        </div>
     </div>
 
     <?php ActiveForm::end(); ?>
