f9c2c935641ce40bcf5776f0c96e6353571cd9ba
amfunNyLORD <funnybulletgames@yandex.ru>
2023-12-23 21:11:17 +0300

    Добавил новые шрифты, поменял вид карточки при поиске

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -29,7 +29,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     <title><?= Html::encode($this->title) ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Kodchasan:wght@600&family=Montserrat:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kodchasan:wght@600&family=Montserrat:wght@400;500;600&display=swap" rel="stylesheet">
     <?php $this->head() ?>
 </head>
 <body class="d-flex flex-column h-100 main">
--- views/site/find.php
+++ views/site/find.php
@@ -24,14 +24,31 @@ if (!empty($user)) {
                 <canvas class="photo__user" id="photo-user" data-photo-path="<?= $photoPath ?>"></canvas>
             </div>
             <div class="card__block-info">
-                <span class="card__block-info-text"><?= Html::encode($user['first_name']) ?>, <?= $user['age'] ?></span>
+                <div>
+                    <span class="card__block-info__name-text"><?= Html::encode($user['first_name']) ?></span>
+                    <span class="card__block-info__age-text">, Возраст</span>
+                </div>
+                <div class="card__block-info__add-info__city">
+                    <img src="../images/icon_house.svg" width="35">
+                    <span class="card__block-info__city__text">Живёт в: Город</span>
+                </div>
+                <div class="card__block-info__add-info__sex">
+                    <img src="../images/icon_profile.svg" width="25">
+                    <span class="card__block-info__sex__text">Пол</span>
+                </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="4" viewBox="0 0 100% 4" fill="none">
+                <path d="M0 2H3000" stroke="#272C28" stroke-width="3"/>
+            </svg>
+            <div class="card__block__additional-info">
+                <span class="card__block__additional-info__text">Бродяга</span>
             </div>
             <div class="card__block-reaction">
                 <button type="submit" class="dislike-button" style="background: none; border: none;">
-                    <img src="images/dislike.png" width="85px" height="85px" style="margin-left: 10%">
+                    <img src="images/dislike.png" width="45px" height="45px" style="margin-left: 10%">
                 </button>
                 <button type="submit" class="like-button" style="background: none; border: none;">
-                    <img src="images/like.png" width="85px" height="85px" style="margin-right: 10%">
+                    <img src="images/like.png" width="45px" height="45px" style="margin-right: 10%">
                 </button>
             </div>
             <div style="margin-bottom: 5%"></div>
