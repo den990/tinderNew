536600f7804b179d5a14c446b7cbe18e26af721f
amfunNyLORD <funnybulletgames@yandex.ru>
2024-01-10 19:29:37 +0300

    Добавил кнопку для выбора предпочтений

--- views/site/index.php
+++ views/site/index.php
@@ -13,6 +13,7 @@ $this->title = 'Finder';
     <h1 class="title">Сервис поиска новых знакомств</h1>
     <?php if (Yii::$app->user->isGuest):?>
     <a href="<?= Yii::$app->urlManager->createUrl(['site/registration']) ?>" class="register-button"><span class="register-button__text">Создать аккаунт</span></a>
+    <a href="#" class="preference-button"><span class="preference-button__text">Мои предпочтения</span></a>
     <?php else: ?>
         <a href="<?= Yii::$app->urlManager->createUrl(['site/find']) ?>" class="register-button"><span class="register-button__text">Найти себе половинку</span></a>
     <?php endif; ?>
